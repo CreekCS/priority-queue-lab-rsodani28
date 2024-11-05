cca6937d842300b71e2a20f70b8b75be18162ac6
Rishabh Sodani <66751046+rsodani28@users.noreply.github.com>
2024-11-05 10:24:23 -0700

    Add files via upload

--- PriorityQueueLab.java
+++ PriorityQueueLab.java
@@ -1,6 +1,23 @@
-// import statements
+import java.util.*;
+import java.io.*;
+
 
 public class PriorityQueueLab {
-  
-}
 
+	public static void main(String[] args) {
+		Scanner in;
+		try {
+		in = new Scanner(new File("src/RandIntegers.txt"));
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for(int i = 0; i < 10; i++) {
+			pq.add(in.nextInt());
+		}
+		for(int i =0; i < 10; i++) {
+			System.out.println(pq.remove());
+		}
+		}catch(Exception e) {
+			System.out.println("Exception:" + e);
+		}
+	}
+
+}
